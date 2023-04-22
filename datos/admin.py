44383cfebc0f3a5f22e8db09af7cb8b322c4ff2e
Fabián Abarca <ensinergia@gmail.com>
2023-04-22 15:43:30 -0600

    Agregar las tablas de bases de datos creadas al sitio de administración

--- datos/admin.py
+++ datos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Marketplace, Committee, Payment
 
 # Register your models here.
+
+admin.site.register(Marketplace)
+admin.site.register(Committee)
+admin.site.register(Payment)
--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Preparation, Storage
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Preparation)
+admin.site.register(Storage)
