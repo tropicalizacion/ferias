bde55dad4eb5968b6e4de5f2f82ee227c51a3aa4
Fabián Abarca <ensinergia@gmail.com>
2023-04-22 15:14:11 -0600

    Modelos de las bases de datos actuales sin migrar

--- datos/models.py
+++ datos/models.py
@@ -1,9 +1,84 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 
+
 class Marketplace(models.Model):
+    """Model definition for Marketplace (la feria).
+
+    The "area" field is a PolygonField, which is a special type of field that
+    can be used to store a polygon geometry. The PolygonField is a subclass of
+    the GeometryField class, which is a subclass of the Field class. This means
+    that it can be used in the same way as any other field in Django.
+    """
+    PROVINCE_CHOICES = [
+        (1, 'San José'),
+        (2, 'Alajuela'),
+        (3, 'Cartago'),
+        (4, 'Heredia'),
+        (5, 'Guanacaste'),
+        (6, 'Puntarenas'),
+        (7, 'Limón'),
+    ]
+
+    marketplace_id = models.CharField(max_length=50, primary_key=True)
+    # General information
     name = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
+    name_alternate = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    opening_hours = models.CharField(max_length=1023)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    area = models.PolygonField(blank=True, null=True)
+    size = models.CharField(max_length=1)
+    province = models.IntegerField(choices=PROVINCE_CHOICES, blank=False, null=False)
+    canton = models.IntegerField(blank=False, null=False)
+    district = models.IntegerField(blank=False, null=False)
+    address = models.TextField(blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True)
+    operator_committee = models.ManyToManyField(
+        'Committee', blank=True, null=True)
+    operator_center = models.CharField(max_length=255)
+    email = models.EmailField(max_length=63)
+    website = models.URLField(max_length=63)
+    opening_date = models.DateField()
+    # Infrastructure
+    fairground = models.BooleanField()
+    indoor = models.BooleanField()
+    toilets = models.BooleanField()
+    parking = models.BooleanField()
+    bicycle_parking = models.BooleanField()
+    drinking_water = models.BooleanField()
+    # Services
+    food = models.BooleanField()
+    drinks = models.BooleanField()
+    handicrafts = models.BooleanField()
+    flowers = models.BooleanField()
+    plants = models.BooleanField()
+    pet_food = models.BooleanField()
+    pet_friendly = models.BooleanField()
+    # Other
+    payment = models.ManyToManyField('Payment')
+    products = models.ManyToManyField('Product')
+
+    def __str__(self):
+        return self.name
+
+
+class Payment(models.Model):
+    """Model definition for Payment."""
+    payment_id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Committee(models.Model):
+    """Model definition for Committee."""
+    committee_id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    """Model definition for Product."""
+    CATEGORY_CHOICES = [
+        (1, 'Legumbres'),
+        (2, 'Tubérculos'),
+        (3, 'Raíces'),
+        (4, 'Cereales'),
+        (5, 'Frutas'),
+        (6, 'Especies'),
+        (7, 'Hortalizas'),
+        (8, 'No convencionales'),
+    ]
+    SEASON_CHOICES = [
+        (0, 'Sin disponibilidad'),
+        (1, 'Poca disponibilidad'),
+        (2, 'Buena disponibilidad'),
+        (3, 'Temporada alta'),
+    ]
+    CENTER_ORIGIN_CHOICES = [
+        (1, '(I) Asia Oriental'),
+        (2, '(II) Subcontinente indio'),
+        (3, '(IIa) Archipiélago indo-malayo'),
+        (4, '(III) Asia Central'),
+        (5, '(IV) Asia Menor y Creciente Fértil'),
+        (6, '(V) Mediterráneo'),
+        (7, '(VI) Abisinia (actual Etiopía)'),
+        (8, '(VII) Mesoamérica'),
+        (9, '(VIII) Región andina tropical'),
+        (10, '(VIIIa) Región chilena'),
+        (11, '(VIIIb) Región brasileña-paraguaya'),
+        (12, 'Sin clasificación'),
+    ]
+
+    product_id = models.AutoField(primary_key=True)
+    product_url = models.CharField(max_length=63, blank=False, null=False)
+    category = models.PositiveIntegerField(blank=False, null=False)
+    scientific_name = models.CharField(max_length=63, blank=False, null=False)
+    scientific_name_variety = models.CharField(
+        max_length=63, blank=False, null=False)
+    common_name = models.CharField(max_length=63, blank=False, null=False)
+    common_name_variety = models.CharField(
+        max_length=63, blank=False, null=False)
+    common_name_variety_alternate = models.CharField(max_length=63, blank=True, null=True)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    icon = models.ImageField(upload_to='icons', blank=True, null=True)
+    center_origin = models.IntegerField(choices=CENTER_ORIGIN_CHOICES, blank=True, null=True)
+    jan = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    feb = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    mar = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    apr = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    may = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    jun = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    jul = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    aug = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    sep = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    oct = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    nov = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    dec = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    nutrition_comment = models.TextField(blank=True, null=True)
+    preparation = models.ManyToManyField('Preparation', blank=True, null=True)
+    storage = models.ManyToManyField('Storage', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Preparation(models.Model):
+    """Model definition for Preparation."""
+
+    preparation_id = models.AutoField(primary_key=True)
+    preparation_name = models.CharField(max_length=63, blank=False, null=False)
+    preparation_description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Storage(models.Model):
+    """Model definition for Storage."""
+
+    storage_id = models.AutoField(primary_key=True)
+    storage_name = models.CharField(max_length=63, blank=False, null=False)
+    storage_description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
