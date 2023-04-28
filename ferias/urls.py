7cdad23eba0cd44ba6a557dbf14a0b90959e566c
Fabián Abarca <ensinergia@gmail.com>
2023-04-28 17:20:00 -0600

    Desplegar imágenes guardadas en la base de datos de productos

--- ferias/urls.py
+++ ferias/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('usuarios/', include('users.urls'), name='usuarios'),
     path('about/', include('about.urls'), name='about'),
     path('blog/', include('blog.urls'), name='blog'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
