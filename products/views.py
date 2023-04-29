8df3df96cd3a0294a0abae181382ebd9bc88ef6e
Fabián Abarca <ensinergia@gmail.com>
2023-04-28 18:34:44 -0600

    Cambios de nombre en vistas y registros de prueba en DB

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 
 def products(request):
     """View function for all products page of site."""
-    return render(request, "productos.html")
+    return render(request, "products.html")
 
 
 def product(request, product_url):
@@ -15,4 +15,4 @@ def product(request, product_url):
     product = get_object_or_404(Product, product_url=product_url)
     context = {"product": product}
 
-    return render(request, "producto.html", context)
+    return render(request, "product.html", context)
