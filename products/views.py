3457e779c0731000428d70baa7e66c34014e2f92
Fabián Abarca <ensinergia@gmail.com>
2023-04-29 11:27:08 -0600

    Modificar los modelos de productos para incluir variedades en nueva tabla

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Variety
 
 # Create your views here.
 
@@ -13,6 +13,10 @@ def product(request, product_url):
     """View function for product page of site."""
 
     product = get_object_or_404(Product, product_url=product_url)
-    context = {"product": product}
+    varieties = Variety.objects.filter(product_url=product_url)
+    context = {
+        "product": product,
+        "varieties": varieties,
+    }
 
     return render(request, "product.html", context)
