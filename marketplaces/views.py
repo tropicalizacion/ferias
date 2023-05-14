487e59a027e7e3e5a8a6c6a16ec8ee515f31211d
Fabián Abarca <ensinergia@gmail.com>
2023-05-14 11:53:47 -0600

    Encontrar las tres ferias más cercanas a la feria de la página visitada

--- marketplaces/views.py
+++ marketplaces/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Marketplace
+from django.contrib.gis.db.models.functions import Distance
 
 # Create your views here.
 
 
 def ferias(request):
     """View function for all ferias page of site."""
-    return render(request, 'ferias.html')
+    return render(request, "ferias.html")
 
 
 def feria(request, marketplace_url):
     """View function for every feria page of site."""
 
     marketplace = get_object_or_404(Marketplace, pk=marketplace_url)
+    closest_marketplaces = (
+        Marketplace.objects.annotate(
+            distance=Distance("location", marketplace.location)
+        )
+        .exclude(pk=marketplace_url)
+        .order_by("distance")[0:3]
+    )
     context = {
-        'marketplace': marketplace,
+        "marketplace": marketplace,
+        "closest_marketplaces": closest_marketplaces,
     }
-    
-    return render(request, 'feria.html', context)
+
+    return render(request, "feria.html", context)
