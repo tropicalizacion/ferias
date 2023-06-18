7a06894fa10a858a5bfcc8a8fafb255a018f8b1b
Mike Mai <maikemai12@hotmail.com>
2023-06-18 00:45:25 -0600

    Actualización de models.py de products - 6
    Quitar Variety (class)
    Quitar Origin (class)
    Agregar nutritional_description

--- products/models.py
+++ products/models.py
@@ -58,62 +58,10 @@ class Product(models.Model):
     oct = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
     nov = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
     dec = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
+    nutritional_description = models.TextField(max_length=127, blank=True, null=True)
 
     def __str__(self):
         return self.common_name
-
-
-class Variety(models.Model):
-    """Model definition for Variety."""
-
-    SEASON_CHOICES = [
-        (0, "Sin disponibilidad"),
-        (1, "Poca disponibilidad"),
-        (2, "Buena disponibilidad"),
-        (3, "Temporada alta"),
-    ]
-
-    variety_id = models.AutoField(primary_key=True)
-    product_url = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True)
-    scientific_name = models.CharField(max_length=63, blank=False, null=False)
-    scientific_name_variety = models.CharField(max_length=63, blank=True, null=True)
-    common_name_variety = models.CharField(max_length=63, blank=True, null=True)
-    common_name_variety_alternate = models.CharField(
-        max_length=127, blank=True, null=True
-    )
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="products", blank=True, null=True)
-    jan = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    feb = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    mar = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    apr = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    may = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    jun = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    jul = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    aug = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    sep = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    oct = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    nov = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-    dec = models.IntegerField(choices=SEASON_CHOICES, blank=True, null=True)
-
-    def __str__(self):
-        return f"({self.product_url}) {self.common_name_variety}"
-
-
-class Origin(models.Model):
-    """Model definition for Center of Origin."""
-
-    code = models.CharField(
-        primary_key=True,
-        max_length=6,
-        help_text="Definición según 'From Vavilov to the Present: A Review' C. Earle Smith, Jr., Economic Botany, Vol. 23, No. 1 (Jan. - Mar., 1969), que va desde I hasta VIII.",
-    )
-    name = models.CharField(max_length=63)
-    description = models.TextField(blank=True, null=True)
-    region = models.PolygonField(blank=True, null=True)
-
-    def __str__(self):
-        return f'{self.code} - {self.name}'
 
 
 class Preparation(models.Model):
