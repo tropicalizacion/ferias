415cb393cb6b26ff52160504b83c9cdb7dce4e8a
Fabián Abarca <ensinergia@gmail.com>
2023-07-06 17:32:15 -0600

    Primer intento de crear formulario

--- website/views.py
+++ website/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-import requests
-import json
+from ferias.forms import MarketplaceForm
+from marketplaces.models import Marketplace
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
-    ip = requests.get('https://api.ipify.org?format=json')
-    ip_data = json.loads(ip.text)
-    res = requests.get('http://ip-api.com/json/'+ip_data["ip"])
-    location_data_one = res.text
-    location_data = json.loads(location_data_one)
-    return render(request, 'index.html', {'data': location_data})
+    if request.method == 'POST':
+        form = MarketplaceForm(request.POST)
+        if form.is_valid():
+            # Look for the marketplace in the database
+            # Get the answer from the form in the field "fairground"
+            fairground = form.cleaned_data['fairground']
+            results = Marketplace.objects.filter(fairground=fairground)
+            return render(request, 'index.html', {'results': results})
+    else:
+        form = MarketplaceForm()
+    
+    context = {
+        "marketplace_form": form
+    }
+    return render(request, 'index.html', context)
 
 def acerca(request):
     return render(request, 'acerca.html')
