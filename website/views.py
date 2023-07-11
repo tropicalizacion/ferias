a1d6f6d851be7e66830b743f63761b564303388b
Fabián Abarca <ensinergia@gmail.com>
2023-07-10 22:54:55 -0600

    Implementación de búsqueda básica por ubicación, horario y amenidades

--- website/views.py
+++ website/views.py
@@ -1,26 +1,47 @@
 from django.shortcuts import render
 from ferias.forms import MarketplaceForm
 from marketplaces.models import Marketplace
-from django.http import HttpResponseRedirect
 from django.db.models import Q
+from django.contrib.gis.db.models.functions import Distance
 
 # Create your views here.
 
 
 def index(request):
     if request.method == "POST":
+        # Search by location
+        location = request.POST.get("location")
+        print(location)
+        if location != "any_location":
+            marketplaces = Marketplace.objects.all().order_by("name")
+        else:
+            if location == "my_location":
+                # TODO: Get user location
+                coordinates = (9.937087255120892, -84.04391604618372)
+            elif location == "some_location":
+                # TODO: Get location from input with Nominatim
+                coordinates = (9.933364850202214, -84.07706364618377)        
+            marketplaces = (
+                Marketplace.objects.annotate(
+                    distance=Distance("location", coordinates)
+                )
+                .order_by("distance")[0:5]
+            )
+        # Search by schedule
+        day = request.POST.get("day")
+        print(day)
+        marketplaces = marketplaces.filter(opening_hours__contains=day)
+        # Search by amenities
         query = Q()
-        
         fairground = request.POST.get("fairground")
         if fairground is not None:
             query &= Q(fairground=fairground)
         indoor = request.POST.get("indoor")
         if indoor is not None:
             query &= Q(indoor=indoor)
-        results = Marketplace.objects.filter(query).order_by('name')
+        marketplaces = marketplaces.filter(query)
         context = {
-            'results': results,
-            'query': query,
+            "marketplaces": marketplaces,
         }
         return render(request, "index.html", context)
     else:
