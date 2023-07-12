4486e4806824146de204c504d7d8afd64c6af289
Andres-Azofeifa <andazo2501@gmail.com>
2023-07-11 20:30:49 -0600

    Se estandariza el estilo del resto de paginas

--- website/views.py
+++ website/views.py
@@ -3,30 +3,64 @@ from ferias.forms import MarketplaceForm
 from marketplaces.models import Marketplace
 from django.db.models import Q
 from django.contrib.gis.db.models.functions import Distance
+#from geopy.geocoders import Nominatim
+import requests
 
 # Create your views here.
 
 
 def index(request):
+
+    r = requests.get('https://get.geojs.io/')
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    ipAdd = ip_request.json()['ip']
+    url = 'https://get.geojs.io/v1/ip/geo/'+ipAdd+'.json'
+    geo_request = requests.get(url)
+    geo_data = geo_request.json()
+
     if request.method == "POST":
         # Search by location
         location = request.POST.get("location")
         print(location)
+        print(geo_data['latitude'])
+        print(geo_data['longitude'])
+        print(geo_data['city'])
+        print(geo_data['country'])
+
+        '''
         if location != "any_location":
             marketplaces = Marketplace.objects.all().order_by("name")
         else:
             if location == "my_location":
-                # TODO: Get user location
-                coordinates = (9.937087255120892, -84.04391604618372)
+                coordinates = (geo_data['latitude'], geo_data['longitude'])
             elif location == "some_location":
                 # TODO: Get location from input with Nominatim
+                #location = Nominatim(user_agent="GetLoc")
+                #getLocation = location.geocode("Input usuario")
+                #coordinates = (getLocation.latitude, getLocation.longitude)
                 coordinates = (9.933364850202214, -84.07706364618377)        
             marketplaces = (
-                Marketplace.objects.annotate(
-                    distance=Distance("location", coordinates)
-                )
-                .order_by("distance")[0:5]
+                Marketplace.objects.annotate(distance=Distance("location", coordinates)).order_by("distance")[0:3]
             )
+        '''
+        if location != "any_location":
+            marketplaces = Marketplace.objects.all().order_by("name")
+        elif location == "my_location":
+            coordinates = (geo_data['latitude'], geo_data['longitude'])
+            marketplaces = (
+                Marketplace.objects.annotate(distance=Distance("location", coordinates)).order_by("distance")[0:3]
+            )
+        elif location == "some_location":
+            #TODO: Get location from input with Nominatim
+            #location = Nominatim(user_agent="GetLoc")
+            #getLocation = location.geocode("Input usuario")
+            #coordinates = (getLocation.latitude, getLocation.longitude)
+            coordinates = (9.933364850202214, -84.07706364618377)        
+            marketplaces = (
+                Marketplace.objects.annotate(distance=Distance("location", coordinates)).order_by("distance")[0:3]
+            )
+        else:
+            print("No hay ferias disponibles")
         # Search by schedule
         day = request.POST.get("day")
         print(day)
