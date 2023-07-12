ed9465662a1bb76195b60f5080e98c3875cd947c
Fabián Abarca <ensinergia@gmail.com>
2023-07-11 18:15:28 -0600

    Crear modelo Announcement para los anuncios en app website

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from marketplaces.models import Marketplace
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    until = models.DateTimeField()
+    marketplace = models.ManyToManyField(Marketplace)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    publisher = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
