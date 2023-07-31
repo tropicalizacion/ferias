3f173ac284d2e5d767dc6be5cc0d2d6258f2bd20
Fabián Abarca <ensinergia@gmail.com>
2023-07-31 16:07:19 -0600

    Crear nueva página de resultados y de búsqueda

--- marketplaces/urls.py
+++ marketplaces/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ferias, name='ferias'),
+    path('buscar/', views.results, name='results'),
     path('<str:marketplace_url>/', views.feria, name='feria'),
 ]
