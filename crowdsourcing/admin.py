a1a67d92eea4eab143eb333b98b2692d07356f78
Fabián Abarca <ensinergia@gmail.com>
2023-08-01 19:28:05 -0600

    Agregar modelo OpeningHoursEdit a admin

--- crowdsourcing/admin.py
+++ crowdsourcing/admin.py
@@ -1,8 +1,9 @@
 from django.contrib.gis import admin
-from .models import MarketplaceEdit, PhotoEdit, OpeningHoursEdit
+from .models import MarketplaceEdit, PhotoEdit, OpeningHoursEdit, ContactEdit
 
 # Register your models here.
 
 admin.site.register(MarketplaceEdit, admin.GISModelAdmin)
 admin.site.register(PhotoEdit)
 admin.site.register(OpeningHoursEdit)
+admin.site.register(ContactEdit)
