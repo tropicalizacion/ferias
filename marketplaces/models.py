46b677d8041b7efb215adf7da87fd361253e1bcd
Fabián Abarca <ensinergia@gmail.com>
2023-08-03 02:55:53 -0600

    Agregar MarketplaceHistory al admin y modificar algunos campos

--- marketplaces/models.py
+++ marketplaces/models.py
@@ -1,5 +1,6 @@
 from django.contrib.gis.db import models
 from products.models import Product
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -48,6 +49,8 @@ class Marketplace(models.Model):
     phone = models.IntegerField(blank=True, null=True)
     email = models.EmailField(max_length=127, blank=True, null=True)
     website = models.URLField(max_length=127, blank=True, null=True)
+    instagram = models.URLField(max_length=127, blank=True, null=True)
+    facebook = models.URLField(max_length=127, blank=True, null=True)
     operator = models.CharField(max_length=255, blank=True, null=True)
     branch = models.CharField(
         choices=BRANCH_CHOICES, max_length=63, blank=True, null=True
@@ -132,7 +135,8 @@ class MarketplaceHistory(models.Model):
         ("surface", "un un espacio amplio de parqueo"),
     ]
 
-    marketplace_url = models.CharField(max_length=50, primary_key=True)
+    marketplace_history_id = models.CharField(max_length=127, primary_key=True)
+    marketplace = models.ForeignKey("Marketplace", on_delete=models.CASCADE)
     # General information
     name = models.CharField(max_length=127)
     name_alternate = models.CharField(max_length=127, blank=True, null=True)
@@ -150,6 +154,8 @@ class MarketplaceHistory(models.Model):
     phone = models.IntegerField(blank=True, null=True)
     email = models.EmailField(max_length=127, blank=True, null=True)
     website = models.URLField(max_length=127, blank=True, null=True)
+    instagram = models.URLField(max_length=127, blank=True, null=True)
+    facebook = models.URLField(max_length=127, blank=True, null=True)
     operator = models.CharField(max_length=255, blank=True, null=True)
     branch = models.CharField(
         choices=BRANCH_CHOICES, max_length=63, blank=True, null=True
@@ -178,10 +184,10 @@ class MarketplaceHistory(models.Model):
     other_services = models.CharField(max_length=255, blank=True, null=True)
     # Products
     products = models.ManyToManyField(Product, blank=True)
-
     # History
     saved_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255, blank=True, null=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    comments_reviewer = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.saved_at})"
