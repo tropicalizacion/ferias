9c4b50b7267c97002314adcb50641900204b716a
WilmerAraya <wilmeraraya21@gmail.com>
2023-11-28 22:20:31 -0600

    Se agregan los textos del website como diccionarios en plantillas

--- website/views.py
+++ website/views.py
@@ -15,18 +15,21 @@ def cover(request):
 
 def index(request):
     # Get the text for the home page where "page" is "/"
+    
     text = Text.objects.filter(page="/")
-    text_hero = text.filter(section="hero")
-    text_saludable = text.filter(section="features", subsection="saludable")
-    text_barato = text.filter(section="features", subsection="barato")
-    text_nuestro = text.filter(section="features", subsection="nuestro")
+    texts = {
+        'hero': text.filter(page="/", section="hero").first().content,
+        'features': {
+            'saludable': text.filter(page="/", section="features", subsection="saludable").first().content,
+            'barato': text.filter(page="/", section="features", subsection="barato").first().content,
+            'nuestro': text.filter(page="/", section="features", subsection="nuestro").first().content
+        },
+    }
+    
     if request.method == "POST":
         marketplaces_match, marketplaces_others, marketplaces_keyword, keyword, query_text, by_location = search_marketplaces(request.POST)
         context = {
-            "text_hero": text_hero,
-            "text_saludable": text_saludable,
-            "text_barato": text_barato,
-            "text_nuestro": text_nuestro,
+            "texts": texts,
             "show_results": True,
             "query_text": query_text,
             "by_location": by_location,
@@ -38,12 +41,8 @@ def index(request):
         return render(request, "index.html", context)
     else:
         context = {
-            "text_hero": text_hero,
-            "text_saludable": text_saludable,
-            "text_barato": text_barato,
-            "text_nuestro": text_nuestro,
+            "texts":texts
         }
-        print(text_hero)
         return render(request, "index.html", context)
 
 
