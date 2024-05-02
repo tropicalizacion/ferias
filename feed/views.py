c41be049565ddc907a8433d7d029855b0250263b
Fabián Abarca <ensinergia@gmail.com>
2024-05-02 15:39:32 -0300

    Configuración sin formato de las páginas de noticias, eventos y alertas

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def feed(request):
+    return render(request, "feed.html")
+
+
+def event(request, event_slug):
+    return render(request, "event.html")
+
+
+def news(request, news_slug):
+    return render(request, "news.html")
+
+
+def alert(request, alert_slug):
+    return render(request, "alert.html")
