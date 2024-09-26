512684b4d26ade4ebdbe6f543f13bf108eb77d3e
Fabián Abarca <ensinergia@gmail.com>
2024-09-26 14:16:48 -0300

    Actualización de textos en página de datos y URLs

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("ferias", views.ferias, name="datos-ferias"),
     path("productos", views.productos, name="datos-productos"),
     path("api/docs/schema/", views.get_schema, name="schema"),
-    path("api/docs/", SpectacularRedocView.as_view(url_name='schema'), name="redoc"),
+    path("api/docs/", SpectacularRedocView.as_view(url_name='schema'), name="docs"),
     path("api/", include(router.urls)),
 ]
