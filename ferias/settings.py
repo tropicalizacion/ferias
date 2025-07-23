c24e2ff5912cc1fcf9373c68219363b34b6c3af8
disotocastgro <juandisoca@gmail.com>
2025-07-23 14:28:26 -0600

    Docker: Modificar Settings.py para que utilice una contraseña, host y port obligatorios para Docker
    Co-authored-by: AleJimenezRJ <alejimenezrj@gmail.com>

--- ferias/settings.py
+++ ferias/settings.py
@@ -100,6 +100,9 @@ DATABASES = {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD")
+        # "HOST": config("DB_HOST"),
+        # "PORT": config("DB_PORT"),
     },
 }
 
