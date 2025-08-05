896655322514014035e8e67a5ec9b25bf85b2d53
disotocastro <juandisoca@gmail.com>
2025-08-04 20:18:21 -0600

    Docker: mover scripts
    Co-authored-by: AleJimenezRJ <alejimenezrj@gmail.com>

---
+++ scripts/dmigrations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker exec ferias_django_app python manage.py makemigrations marketplaces
+docker exec ferias_django_app python manage.py makemigrations products
+docker exec ferias_django_app python manage.py makemigrations crowdsourcing
+docker exec ferias_django_app python manage.py makemigrations website
+docker exec ferias_django_app python manage.py makemigrations users
+docker exec ferias_django_app python manage.py makemigrations feed
+docker exec ferias_django_app python manage.py makemigrations blog
+docker exec ferias_django_app python manage.py makemigrations content
+docker exec ferias_django_app python manage.py makemigrations recipes
+docker exec ferias_django_app python manage.py migrate
+
+docker exec ferias_django_app python manage.py loaddata marketplaces
+docker exec ferias_django_app python manage.py loaddata products
+docker exec ferias_django_app python manage.py loaddata crowdsourcing
+docker exec ferias_django_app python manage.py loaddata users
+docker exec ferias_django_app python manage.py loaddata feed
+docker exec ferias_django_app python manage.py loaddata content
