1168c1d43f987da3d70eaa148ce3819d91230d57
disotocastro <juandisoca@gmail.com>
2025-08-06 11:32:14 -0600

    Docker: Modificar Migraciones

--- scripts/dmigrations.sh
+++ scripts/dmigrations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker exec ferias_django_app python manage.py migrate
 docker exec ferias_django_app python manage.py makemigrations marketplaces
 docker exec ferias_django_app python manage.py makemigrations products
 docker exec ferias_django_app python manage.py makemigrations crowdsourcing
@@ -6,12 +7,13 @@ docker exec ferias_django_app python manage.py makemigrations website
 docker exec ferias_django_app python manage.py makemigrations users
 docker exec ferias_django_app python manage.py makemigrations feed
 docker exec ferias_django_app python manage.py makemigrations blog
-docker exec ferias_django_app python manage.py makemigrations content
+# docker exec ferias_django_app python manage.py makemigrations content
 docker exec ferias_django_app python manage.py makemigrations recipes
 docker exec ferias_django_app python manage.py migrate
 
 docker exec ferias_django_app python manage.py loaddata marketplaces
 docker exec ferias_django_app python manage.py loaddata products
+docker exec ferias_django_app python manage.py loaddata website
 docker exec ferias_django_app python manage.py loaddata crowdsourcing
 docker exec ferias_django_app python manage.py loaddata users
 docker exec ferias_django_app python manage.py loaddata feed
